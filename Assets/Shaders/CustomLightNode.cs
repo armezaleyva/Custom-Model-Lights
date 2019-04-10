81f9b243a3c9d0d7b34f0744a648fa858749e4db
Armando Meza Leyva <armezaleyva@gmail.com>
2019-04-10 08:51:06 -0700

    xd

---
+++ Assets/Shaders/CustomLightNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.ShaderGraph;
+
+public class CustomLightNode : CodeFunctionNode
+{
+    public override bool hasPreview { get => false; }
+
+    public override MethodInfo GetFunctionToConvert()
+    {
+        
+    }
+}
